f0fbf189caa605aa148bcd5820f6c911800f063e
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2024-05-09 13:46:34 +0900

    .

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://www.conun.io",
+  match: "https://www.conun.io/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
